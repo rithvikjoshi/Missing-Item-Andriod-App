ed65561fa961be0ced1c7c3941c092e7bdac5575
rithvikjoshi <joshirithvik@gmail.com>
2022-01-14 16:23:22 +0530

    First Commit

---
+++ Missing items/app/src/main/java/com/example/lostandfound/TwoButtonsActivity.java
@@ -0,0 +1,84 @@
+package com.example.lostandfound;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class TwoButtonsActivity extends AppCompatActivity {
+  private Button myLostButton;
+  private Button myFoundButton;
+
+  private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two_buttons_layout);
+
+        myLostButton=(Button)findViewById(R.id.LostButton);
+        myFoundButton=(Button)findViewById(R.id.FoundButton);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        myLostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(TwoButtonsActivity.this,LostPageActivity.class);
+                intent.putExtra("from","LostButton");
+                startActivity(intent);
+                finish();
+            }
+        });
+        myFoundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(TwoButtonsActivity.this,LostPageActivity.class);
+                intent.putExtra("from","FoundButton");
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.action_signout){
+             mAuth.signOut();
+             startActivity(new Intent(TwoButtonsActivity.this,MainActivity.class));
+             finish();
+        }
+
+        if(item.getItemId()== R.id.action_myaccount){
+            startActivity(new Intent(TwoButtonsActivity.this,MyAccountActivity.class));
+            finish();
+        }
+
+        if(item.getItemId()== R.id.action_aboutus){
+            startActivity(new Intent(TwoButtonsActivity.this, AboutUs.class));
+            finish();
+        }
+
+        if(item.getItemId()== R.id.action_appinfo){
+            startActivity(new Intent(TwoButtonsActivity.this, AppInfo.class));
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Missing-Item-Andriod-App
+We are implementing this project for the B.M.S College of Engineering,
+Bangalore. Because of this app students, faculty, and college staff will find their
+stuff very easily. The app will automatically capture your email, which is given
+when you register. To get a view of the lost item posted, one must report their lost
+item, if the is data matching, then only that lost item is viewed. This feature is
+used to avoid invalid claims.
